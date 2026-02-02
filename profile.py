dd1e69a083137c76ce579a66c19488319405033b
NoahNzeki <noahnzeki@gmail.com>
2026-02-02 19:41:26 +0200

    Modifications

--- profile.py
+++ profile.py
@@ -46,7 +46,7 @@ with st.sidebar:
     st.markdown("## 📫 Contact")
     st.markdown("📧 noahnzeki@yandex.com")
     st.markdown("## 💻 GitHub")
-    st.markdown("[🐙 GitHub](https://github.com/nnw)")
+    st.markdown("🐙 [GitHub](https://github.com/nnw)")
 
 # Main layout
 col1, col2 = st.columns([1, 2])
